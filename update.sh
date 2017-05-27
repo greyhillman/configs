fe07c3b6ae26deb2d2f3071a0d6b0c83488170d0
Grey Hill <infogreytech@gmail.com>
2017-05-27 14:28:47 -0700

    Save point

--- update.sh
+++ update.sh
@@ -5,9 +5,9 @@ cp ~/.vimrc .
 cp ~/.tmux.conf .
 cp ~/.bash_aliases .
 
-# Add all the files to be tracked by git
-git add -A
-
 # Show what has changed so hopefully nothing bad is committed
 git diff
+
+# Add all the files to be tracked by git
+git add -A
 
