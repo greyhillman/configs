585b0408066ce93e0108b148112f2ed29e2b6819
Grey Hill <infogreytech@gmail.com>
2017-05-27 15:36:29 -0700

    Commit

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# Add the program that shows various system stats
+# Like CPU, RAM, Network, and disk on the top bar
+sudo add-apt-repository ppa:indicator-multiload/stable-daily
+sudo apt update
+sudo apt install indicator-multiload
+
+
--- update.sh
+++ update.sh
@@ -4,6 +4,8 @@
 cp ~/.vimrc .
 cp ~/.tmux.conf .
 cp ~/.bash_aliases .
+cp ~/nonotifications.sh .
+cp ~/disable_mouse_acceleration.sh
 
 # Show what has changed so hopefully nothing bad is committed
 git diff
